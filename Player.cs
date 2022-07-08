4b13616b2c77f7ac646037c2c11a717f71e6e528
UtkarshGaikwad11 <108927005+UtkarshGaikwad11@users.noreply.github.com>
2022-07-08 15:39:58 +0530

    Second Commit
    this is second commit

---
+++ Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisionDemoApp
+{
+    public class Player : IComparable
+    {
+        public string Name { get; set; }
+        public int Runs { get; set; }
+        public int Age { get; set; }
+        public int Rank { get; set; }
+        int IComparable.CompareTo(object obj)
+        {
+            Player temp = (Player)obj;
+            if (this.Age > temp.Age)
+                return 1;
+            if (this.Age < temp.Age)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
